e72fa3b49f2b060c71e73ad0075853801f7d9509
drewk93 <141455844+drewk93@users.noreply.github.com>
2023-10-04 05:47:46 -0800

    Update coffeeMaker.js

--- coffeeMaker.js
+++ coffeeMaker.js
@@ -1,21 +1,25 @@
 let coffeeMaker = {
     coffeeHot: true,
-    coffeeVolume: 8,
-    coffeeFull: false,
+    coffeeVolume: 10,
+    coffeeFull: true,
     isCoffeeFull: function (volume){
         if (coffeeMaker.coffeeVolume === 10){
             coffeeMaker.coffeeFull = true;
-            console.log(coffeeMaker.coffeeFull)
+            
         } else {
-        coffeeFull = false;
-        console.log(coffeeMaker.coffeeFull)
+        coffeeFull = false;        
         }
+        console.log(coffeeMaker.coffeeFull)
     },
     brewCoffee: function (){
         coffeeMaker.coffeeVolume = 10;
         return `Coffee Brewed to ${coffeeMaker.coffeeVolume} cups!`;
+    },
+    drinkCoffee: function(volume){
+        coffeeMaker.coffeeVolume -= 1
+        console.log(`You drank 1 cup of coffee. Coffee volume is now ${coffeeMaker.coffeeVolume} cups.`)
     }
 }
-
-console.log(coffeeMaker.isCoffeeFull())
-console.log(coffeeMaker.brewCoffee(),coffeeMaker.isCoffeeFull())
+console.log(coffeeMaker.coffeeFull)
+console.log(coffeeMaker.drinkCoffee())
+console.log(coffeeMaker.coffeeFull)
